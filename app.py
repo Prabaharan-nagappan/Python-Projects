885f12a49c7c4da5510567a5c7f30fe64f3bb4a9
Prabaharan Nagappan <126092038+Prabaharan-nagappan@users.noreply.github.com>
2024-01-08 18:06:50 +0000

    version 0

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-tasks = []
-
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', tasks=tasks)
+    weather_data = None
 
+    if request.method == 'POST':
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        weather_data = get_weather_data(latitude, longitude, start_date, end_date)
 
-@app.route('/add', methods=['POST'])
-def add():
-    task = request.form.get('task')
-    tasks.append(task)
-    return redirect(url_for('index'))
+    return render_template('index.html', weather_data=weather_data)
 
+def get_weather_data(latitude, longitude, start_date, end_date):
+    api_url = f'https://archive-api.open-meteo.com/v1/era5?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m'
+    
+    response = requests.get(api_url)
+    weather_data = response.json()
 
-@app.route('/delete/<int:task_id>')
-def delete(task_id):
-    if 0 <= task_id < len(tasks):
-        del tasks[task_id]
-    return redirect(url_for('index'))
-
+    return weather_data['hourly']
 
 if __name__ == '__main__':
     app.run(debug=True)
